febb71b5c2729b5123d0628dcd6e47dbc409da85
Jerkwater-Safety <bte1118@gmail.com>
2026-02-16 14:06:26 -0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import SafetyOS from './SafetyOS'
+import SafetyOS from './SafetyOS.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
